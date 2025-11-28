a6b37ea6f11378364bce7410f3ca67058417fe77
BruceLitvin <brucel15@nycstudents.net>
2025-11-28 18:02:02 -0500

    IDK if what im doing is right but I may be on to something

---
+++ src/Planner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Planner {
+    private String item;
+    private int listTotal;
+String[] itemList= new String[listTotal];
+
+
+
+    public String addItem(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("What would you like to add?");
+
+    if (s.toString().equals(item));
+
+
+
+        return null;
+    }
+
+
+
+}
