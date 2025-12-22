176d4ee7ccd0e8c2cffda1546dfcd42e05eb54c4
BruceLitvin <brucel15@nycstudents.net>
2025-12-22 17:21:56 -0500

    Pain

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,42 @@
+import java.util.Scanner;
 public class Main{
 
     public static void main(String[] args) {
         Planner f= new Planner();
-       // f.add();
-        //f.add( "homework", 6);
-        //f.result();
-       // f.remove();
-        //f.remove( 6);
-        //f.result();
-       f.goalPlace("hello");
-        f.goalPlace("hello");
-        f.goalPlace("hello");
-        f.goalRemove();
+       boolean lock = true;
+        System.out.println("Key: EventA to add goal");
+        System.out.println("EventB to remove a goal");
+        System.out.println("GoalA to add goal");
+        System.out.println("GoalR to remove goal");
+        System.out.println("End to cut program");
+       while(lock) {
+           Scanner a = new Scanner(System.in);
+           System.out.println("What would you like to do");
+           String activity = a.next();
+           if(activity.equals("EventA")){
+               f.add();
+           }
+           else if(activity.equals("EventR")){
+               f.remove();
+           }
+           else if(activity.equals("GoalA")){
+               f.goalInput();
+           }
+           else if(activity.equals("GoalR")){
+               f.goalRemove();
+           }
+           else if(activity.equals("End")){
+               lock= false;
 
+           }
+           else if(activity.equals("end")){
+               lock= false;
+
+           }
+
+
+
+       }
     }
 
 
